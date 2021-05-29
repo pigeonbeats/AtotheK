dacd50520b5b1d8747a2cd9820a27e89b8c369a7
pigeonbeats <82282656+pigeonbeats@users.noreply.github.com>
2021-05-29 16:35:13 +1000

    Update CSSKern.js

--- CSSKern/CSSKern.js
+++ CSSKern/CSSKern.js
@@ -7,6 +7,6 @@ function setup () {
   editablevariablename = createP('CA'); // createP creates a paragraph element that prints the text in the quotes under the name editablevariablename, using createP() instead of text() enables us to add CSS styles to the text, which is what is needed to read the font file's kerning information. 
   editablevariablename.style('font-size' ,'100px'); // using .style after the paragraph element is what gives it CSS styles, look them up for more options
   editablevariablename.style('font-family', 'testfont');
-  editablevariablename.style('font-kerning', 'normal'); // this is what enables font kerning in your text
+  editablevariablename.style('font-kerning', 'normal'); // this is what enables font kerning in your text. technically you don't need this because using a paragraph element automatically adds kerning to your text, but this line is here for visibility. 
   editablevariablename.position(0, 0);
 }
