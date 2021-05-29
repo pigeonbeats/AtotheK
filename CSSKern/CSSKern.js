6d084f2457c8950749434297ad0a6cf6820a7c87
pigeonbeats <82282656+pigeonbeats@users.noreply.github.com>
2021-05-29 15:42:21 +1000

    Add files via upload

---
+++ CSSKern/CSSKern.js
@@ -0,0 +1,12 @@
+//IMPORTANT: the sketch needs to be saved after every edit for the code to run properly (thanks sylvain for the heads up)
+//there are edits to the index.html as well as a new file 'style.css' that are essential for this to work
+
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  editablevariablename = createP('CA'); // createP creates a paragraph element that prints the text in the quotes under the name editablevariablename, using createP() instead of text() enables us to add CSS styles to the text, which is what is needed to read the font file's kerning information. 
+  editablevariablename.style('font-size' ,'100px'); // using .style after the paragraph element is what gives it CSS styles, look them up for more options
+  editablevariablename.style('font-family', 'testfont');
+  editablevariablename.style('font-kerning', 'normal'); // this is what enables font kerning in your text
+  editablevariablename.position(0, 0);
+}
