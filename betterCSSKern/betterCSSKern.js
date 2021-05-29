07fce3e1bfff7142e5b7490593d01ccd67a3b760
pigeonbeats <82282656+pigeonbeats@users.noreply.github.com>
2021-05-29 16:07:48 +1000

    Add files via upload

---
+++ betterCSSKern/betterCSSKern.js
@@ -0,0 +1,10 @@
+//IMPORTANT: the sketch needs to be saved after every edit for the code to run properly (thanks sylvain for the heads up)
+//there are edits to the index.html as well as a new file 'style.css' that are essential for this to work
+
+function setup () {
+ 
+}
+
+function draw () {
+
+}
